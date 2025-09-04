97c34762abad29a505ff9975d3aca0a705b67c70
Kovirum <kovirum@mail.ru>
2025-09-04 17:43:29 +0300

    исправлены баги

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 def math_roots(a, b, c):
-    d = b ** 22 - 4 * a * c
+    d = b ** 2 - 4 * a * c
     print(d)
 
     if d > 0:
         print("Дискриминант больше 0 - 2 действительных корня")
-        print(f"x1 = {-b-d**0.5/(2*a)}, x2 = {-b+d*0.5/(2*a)}")
+        print(f"x1 = {-b-d**0.5/(2*a)}, x2 = {-b+d**0.5/(2*a)}")
     elif d == 0:
         print("Дискриминант равен 0 - 1 действительный корень")
-        print(f"x = {-b/2*a}")
+        print(f"x = {-b/(2*a)}")
     else:
         print("Дискриминант меньше 0 - действительных корней нет")
         return
@@ -18,5 +18,5 @@ a, b, c = map(int, input().split(' '))
 
 print(f"{a=} {b=} {c=}")
 
-math_roots(c, b, a)
+math_roots(a, b, c,)
 
